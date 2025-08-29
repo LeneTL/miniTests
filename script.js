d0448e8780225df6eec01181c63028d7c49d701d
Lene <Lene-tl@hotmail.com>
2025-08-29 14:38:07 +0200

    Week3assigments

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function restoreDivColor() {
   document.getElementById("mydiv").classList.remove("red");
 }
 
+// -----------------------------------------------
+
 var outputDiv = document.getElementById('output');
 var shownNumber = 0;
 
@@ -25,4 +27,27 @@ outputDiv.innerHTML = shownNumber;
 function subtractNumber(){
 shownNumber --;
 outputDiv.innerHTML = shownNumber;
+}
+
+// -----------------------------------------------
+
+function changeTextAndColourAndSize(color,width,height,text){
+  document.getElementById('changingDiv').innerHTML = /*html*/`
+  <div style="background-color:${color}; width:${width}px; height:${height}px;">${text}</div>
+`;}
+// -----------------------------------------------
+
+let selectedColour= null;
+
+
+function selectBodyColour(color){
+  this_element_.style.border = '1px solid red'
+  selectedColour= color;
+  document.getElementById(borderOn).style.border = '#090909 1px solid;'
+ }
+
+function applyBodyColour(){
+  if (selectedColour) {
+    document.body.style.background = selectedColour
+  }
 }
